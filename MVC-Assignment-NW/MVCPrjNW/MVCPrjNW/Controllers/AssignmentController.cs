7b53dc583c9684223e61080a55cd5f76cd9dc373
frontend-centuroglobal <frontend@centuroglobal.com>
2023-02-12 12:45:16 +0530

    MVC Assignment-1 Added

---
+++ MVC-Assignment-NW/MVCPrjNW/MVCPrjNW/Controllers/AssignmentController.cs
@@ -0,0 +1,39 @@
+﻿using MVCPrjNW.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+
+namespace MVCPrjNW.Controllers
+{
+    public class AssignmentController : Controller
+    {
+        NorthwindEntities NE = new NorthwindEntities();
+        // GET: Assignment
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetCustomerResidingInGermany()
+        {
+            var Country = ("GetCustomerResidingInGermany", NE.Customers.Where(x => x.Country == "Germany").ToList());
+            return View(Country);
+
+        }
+
+        public ActionResult GetCustByOrderId()
+        {
+            // var ord = ("GetCustByOrderId", NE.Orders.Where(o => o.OrderID == 10248).ToList());
+            var ord = from o in NE.Orders
+                      where o.OrderID == 10248
+                      select o;
+            return View(ord);
+        }
+
+
+    }
+}
---
+++ MVC-Assignment-NW/MVCPrjNW/MVCPrjNW/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCPrjNW.MvcApplication" Language="C#" %>
